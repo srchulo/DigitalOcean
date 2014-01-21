14d8fd7c69dc308acdee2b2196e3cd03f28d760b
Adam Hopkins <violapiratejunky@gmail.com>
2014-01-20 22:44:40 -0600

    updated Changes and changed required module from MooseX::Types to MouseX::Types

--- DigitalOcean/lib/DigitalOcean.pm
+++ DigitalOcean/lib/DigitalOcean.pm
@@ -103,11 +103,11 @@ DigitalOcean - An OO interface to the DigitalOcean API.
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
