8ab7fa896cb4fd3bfe1a4f0028fdbcafdb2f6f02
Adam Hopkins <srchulo@Adams-MBP.atlanticbb.net>
2015-06-13 23:40:53 -0400

    test for retrieving droplet

--- test.pl
+++ test.pl
@@ -47,6 +47,12 @@ exit;
 =cut
 my $do = DigitalOcean->new(oauth_token => 'a4a582d91e8585d481f1c4388c73e66a7c299ffbbaeffd85d54cb03db502eb9c');
 
+my $droplet = $do->droplet(207673);
+
+print Data::Dumper->Dump([$droplet]);
+exit;
+
+
     my $new_droplet = $do->create_droplet(
         name => 'new-droplet',
         region => 'sfo1',
