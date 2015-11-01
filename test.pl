c4f1561fdcef28ba2e2545b00135258947c12042
Adam Hopkins <srchulo@Adams-MacBook-Pro.local>
2015-11-01 02:24:23 -0800

    .

--- test.pl
+++ test.pl
@@ -45,7 +45,7 @@ print $net->v4->[0]->gateway;
 
 exit;
 =cut
-my $do = DigitalOcean->new(oauth_token => 'a4a582d91e8585d481f1c4388c73e66a7c299ffbbaeffd85d54cb03db502eb9c');
+my $do = DigitalOcean->new(oauth_token => '');
 
 
 
