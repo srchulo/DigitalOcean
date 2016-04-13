f656fee1cbd9c23d865e8876757a80d690b46384
Adam Hopkins <violapiratejunky@gmail.com>
2016-04-13 14:05:07 -0700

    Update DigitalOcean.pm

--- lib/DigitalOcean.pm
+++ lib/DigitalOcean.pm
@@ -297,19 +297,12 @@ sub _request {
         my $json_coder = JSON::XS->new->ascii->allow_nonref;
         my $req_body = $json_coder->encode($req_body_hash);
         $req->content($req_body);
-
-        print "REQ BODY $req_body\n";
     }
 
     my $response = $self->ua->request($req);
     my $json;
     if($response->content) {
         $json = JSON::XS->new->utf8->decode($response->content);
-
-        #TEMPORARY
-        my $coder = JSON::XS->new->ascii->pretty->allow_nonref;
-        my $pretty_printed_unencoded = $coder->encode ($json);
-        print "$pretty_printed_unencoded\n";
 
         #die with DigitalOcean::Error
         if($response->code < 200 or $response->code >= 300) {
@@ -343,8 +336,6 @@ sub _request {
 
             #if actions array is present and we are supposed to wait on events, then wait!
             if($json->{links}->{actions} and ($self->wait_on_actions or $wait_on_action)) { 
-                print "WAITING on ACTION in request\n";
-
                 #wait on each returned action that occurred from the API call
                 for my $act_temp (@{$json->{links}->{actions}}) { 
                     my $action = $self->action($act_temp->{id});
